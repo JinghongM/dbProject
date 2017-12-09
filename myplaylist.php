93b26d7ea1454641d1552625fb27bbc284afba5f
LinyaGong <lg2740@nyu.edu>
2017-12-09 04:56:09 -0500

    Add files via upload

---
+++ myplaylist.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Play List</title>
+    <link href="./css/profile.css" rel="stylesheet" media="screen">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/following.css">
+    <style type="text/css">
+      body, html{
+     height: 100%;
+    background:url(https://i.ytimg.com/vi/4kfXjatgeEU/maxresdefault.jpg);
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    font-family: 'Oxygen', sans-serif;
+        background-size: cover;
+}
+    </style>
+    <script src="./jquery/jquery.min.js"></script> 
+
+</head>
+<body>
+  <?php  
+        include 'databases.php';
+
+        $vdelete = 0;
+        $vadd = 0;
+
+        if(isset($_GET['playlistid'])) {
+          $vlist = 0;
+          $listid = $_GET['playlistid'];
+          $Username = $_GET['user'];
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          $sql1 = "SELECT Username as name FROM playlist WHERE playlistID='$listid'";
+          $result1 = $conn->query($sql1);
+          while($row=mysqli_fetch_array($result1)){
+            if($Username == $row['name']) {
+              $vdelete = 1;
+            } else {
+              $vadd = 1;
+            }
+          }
+        } else {
+          $vlist = 1;
+          $listid = $_GET['albumid'];
+          $vadd = 1;
+        }           
+
+        if(isset($_GET['delete'])) {
+          $Username = $_GET["user"];
+          $delete = $_GET['delete'];
+          $listid = $_GET['playlistid'];
+          include 'databases.php';
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          $sql1 = "DELETE FROM playlisttrack WHERE playlistid='$listid' AND trackid='$delete'";
+          $conn->query($sql1);
+        }
+
+        if(isset($_POST['delform'])){
+          $array = $_POST['selector'];
+          $Username = $_GET["user"];
+          $listid = $_GET['playlistid'];
+          include 'databases.php';
+          $conn = new mysqli($servername, $username, $password, $dbname);
+          foreach ($array as $delete) {
+            $sql1 = "DELETE FROM playlisttrack WHERE playlistid='$listid' AND trackid='$delete'";
+            $conn->query($sql1);
+          }
+       }
+?>
+
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+     <?php
+        $Username = $_GET["user"];
+        echo '<a class="navbar-brand" href="welcome.php?user='.$Username.'">Spotify</a>';
+      ?>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <?php
+             $Username = $_GET["user"];
+             echo '<a class="nav-link" href="profile.php?user='.$Username.'">Profiles<span class="sr-only">(current)</span></a>';
+
+             ?>
+             
+<!--             <a class="nav-link" href="profile.php">Profiles<span class="sr-only">(current)</span></a>
+ -->       </li>
+          <li class="nav-item active">
+                        <?php
+             $Username = $_GET["user"];
+             echo '<a class="nav-link" href="following.php?user='.$Username.'">Following<span class="sr-only">(current)</span></a>';
+
+             ?>
+             <!-- 
+            <a class="nav-link" href="Follows.html">Following<span class="sr-only">(current)</span></a> -->
+          </li>
+          <li class="nav-item active">
+            <?php
+             $Username = $_GET["user"];
+             echo '<a class="nav-link" href="followee.php?user='.$Username.'">Follower<span class="sr-only">(current)</span></a>';
+
+             ?>
+          </li>
+          <li class="nav-item active">
+            <?php
+             $Username = $_GET["user"];
+             echo '<a class="nav-link" href="likes.php?user='.$Username.'">Artists<span class="sr-only">(current)</span></a>';
+
+             ?>
+          </li>
+          <li class="nav-item active">
+            <?php
+             $Username = $_GET["user"];
+             echo '<a class="nav-link" href="playlist.php?user='.$Username.'">Playlists<span class="sr-only">(current)</span></a>';
+
+             ?>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Playlists</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul> -->
+      </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+
+    
+    <main role="main">
+<div style="background:transparent" class="jumbotron">
+           <div class="container">
+           <div class="row user-menu-container square">
+          <div class="row coralbg white">
+                <div class="col-md-6 no-pad">
+                    <div class="user-pad">
+                        <h3>
+                          <?php
+                            include 'databases.php';
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            if ($conn->connect_error) {
+                              die("Connection failed: " . $conn->connect_error);
+                            }
+                            if ($vlist == 0){
+                              $sql1 = "SELECT ptitle FROM playlist WHERE playlistID='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["ptitle"];
+                              }
+                            } else {
+                              $sql1 = "SELECT atitle FROM album WHERE albumID='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["atitle"];
+                              }
+                            }
+                            echo $cid;
+                          ?>
+                        </h3>
+                    </div>
+                    <div class="user-pad">
+                          <?php
+                            include 'databases.php';
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            if ($conn->connect_error) {
+                              die("Connection failed: " . $conn->connect_error);
+                            }
+                            if ($vlist == 0){
+                              $sql1 = "SELECT count(*) as num FROM playlisttrack WHERE playlistid='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["num"];
+                              }
+                            } else {
+                              $sql1 = "SELECT count(*) as num FROM albumtrack WHERE albumID='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["num"];
+                              }
+                            }
+                            echo "Number of tracks:".$cid;
+                          ?>
+                    </div>
+                    <div class="user-pad">
+                          <?php
+                            include 'databases.php';
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            if ($conn->connect_error) {
+                              die("Connection failed: " . $conn->connect_error);
+                            }
+                            if ($vlist == 0){
+                              $sql1 = "SELECT ReleaseDate FROM playlist WHERE playlistID='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["ReleaseDate"];
+                              }
+                            } else {
+                              $sql1 = "SELECT IssueDate FROM album WHERE albumID='$listid'";
+                              $result1 = $conn->query($sql1);
+                              while($row=mysqli_fetch_array($result1)){
+                                $cid=$row["IssueDate"];
+                              }
+                            }
+                            echo "ReleaseDate:".$cid;
+                          ?>
+                    </div>
+                    </div>
+                <div class="col-md-6 no-pad">
+                    <div class="user-image">
+                        <img src="https://farm7.staticflickr.com/6163/6195546981_200e87ddaf_b.jpg" class="img-responsive thumbnail">
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+      </div>
+
+    <?php
+      if ($vdelete == 1) {
+        echo'<form action="myplaylist.php?user='.$Username.'&playlistid='.$listid.'" method="post">'; 
+      } elseif ($vlist == 0){
+        echo'<form action="addtoplaylist.php?user='.$Username.'&playlistid='.$listid.'" method="post">';
+      } else {
+        echo'<form action="addtoplaylist.php?user='.$Username.'&albumid='.$listid.'" method="post">';
+      }
+    
+    ?>
+
+    <div class="chk-all">
+      <input type="checkbox" id="checkAll" />
+        <div class="btn-group">
+          <a data-toggle="dropdown"  class="btn mini all" aria-expanded="false" id="checkAll">All</a>
+          <?php
+            if ($vdelete == 1) {
+              echo '<button type="submit" class="btn btn-danger" style="visibility: hidden;" id="unfollowAll" name="delform" value="Unfollow Checked">Delete selected</button>';
+            } else {
+              echo '<button type="submit" class="btn btn-danger" style="visibility: hidden;" id="unfollowAll1" name="addform" value="Unfollow Checked">Add all to my playlist</button>';
+            }
+          ?>     
+            </div>
+                             </div>
+                           <table class="table table-inbox table-hover">
+                            <tr>
+                              <td class="inbox-small-cells">
+                                <input type="checkbox"  style="visibility: hidden;">
+                                </td>
+                                <td class="view-message  dont-show">Ttitle</td>
+                                <td class="view-message  text-right">duration</td>
+                                <td class="view-message  inbox-small-cells"><i class="fa fa-paperclip"></i></td>
+                                <td class="view-message text-right">genre</td>
+                            </tr>
+                            <tbody>
+                             <?php 
+                             include 'databases.php';
+                             function changeAuthorization($Authorization)
+                             {
+                              if($Authorization == "private")
+                              {
+                                return "For yourself only";
+                              }
+                              else
+                              {
+                                return "For everyone";
+                              }
+                             }
+                                  $conn = new mysqli($servername, $username, $password, $dbname);
+                                  if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                  }
+                                  if ($vlist == 0) {
+                                    $sql1 = "SELECT track.Ttitle, track.duration, track.genre, track.TrackID
+                                             FROM track,playlisttrack
+                                             WHERE playlisttrack.playlistid='$listid' and track.TrackID=playlisttrack.trackid";
+                                    $result1 = $conn->query($sql1);
+                                  } else {
+                                    $sql1 = "SELECT track.Ttitle, track.duration, track.genre, track.TrackID
+                                             FROM track,albumtrack
+                                             WHERE albumtrack.albumID='$listid' and track.TrackID=albumtrack.trackID";
+                                    $result1 = $conn->query($sql1);
+                                  }
+                                  $numrow = 1;
+                                  while($row = $result1->fetch_assoc())
+                                  {
+                                    echo '<tr>
+                                      <td class="inbox-small-cells">
+                                        <input name="selector[]" type="checkbox" class="mail-checkbox" value="'.$row["TrackID"].'" />
+                                      </td>
+                                      <td class="view-message  dont-show">'.$row["Ttitle"].'</td>
+                                      <td class="view-message  text-right">'.$row["duration"].'</td>
+                                      <td class="view-message  inbox-small-cells"><i class="fa fa-paperclip"></i></td>
+                                      <td class="view-message text-right">'.$row["genre"].'</td>
+                                      <td class="view-message  inbox-small-cells"><i class="fa fa-paperclip"></i></td>
+                                   
+                                      <td>
+                                      <a href="play.php?user='.$Username.'&trackid='.$row["TrackID"].'"><button type="button" class="btn btn-warning">Play</button></a>';
+                                    if ($vadd == 1) {
+                                      echo '<a href="add.php?user='.$Username.'&trackid='.$row["TrackID"].'"><button type="button" class="btn btn-warning">Add</button></a>';
+                                    }
+                                    if ($vdelete == 1) {
+                                      echo '<a href="myplaylist.php?user='.$Username.'&delete='.$row["TrackID"].'&playlistid='.$listid.'"><button type="button" class="btn btn-danger">Delete</button></a>';
+                                    }
+                                    echo '</td></tr>';
+                                   $numrow++;
+                                 }
+                              ?>
+                          </tbody>
+                          </table>
+
+</div>
+</main>
+ <script type="text/javascript">
+$("#checkAll").click(function () {
+  $(".mail-checkbox").prop('checked', $(this).prop('checked'));
+  $("#unfollowAll").css("visibility","visible");
+  $("#unfollowAll1").css("visibility","visible")
+});
+$('#checkAll').change(function() {
+   if ( ! this.checked) {
+        $("#unfollowAll").css("visibility","hidden");
+        $("#unfollowAll1").css("visibility","hidden")
+   }
+
+});
+$(".mail-checkbox").change(function(){
+    if ($('.mail-checkbox:checked')) {
+       //do something
+         $("#unfollowAll").css("visibility","visible");
+         $("#unfollowAll1").css("visibility","visible")
+
+    }
+    if ($('.mail-checkbox:checked').length ==0)
+    {
+        $("#unfollowAll").css("visibility","hidden");
+        $("#unfollowAll1").css("visibility","hidden")
+
+    }
+});
+$(function(){
+      $('#unfollowAll').click(function(){
+        var val = [];
+        $(':checkbox:checked').each(function(i){
+          val[i] = $(this).val();
+        });
+      });
+      $('#unfollowAll1').click(function(){
+        var val = [];
+        $(':checkbox:checked').each(function(i){
+          val[i] = $(this).val();
+        });
+      });
+    });
+
+</script>
+</body>
+</html>
